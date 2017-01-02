eafba31bd65c691edf6f6f3331cca32dbf3f6339
ttae00 <xodbd9756@naver.com>
2017-01-02 15:41:38 +0900

    lst commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+/**
+ * 
+ * HelloWorld 예제
+ * @author 태영
+ *
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
